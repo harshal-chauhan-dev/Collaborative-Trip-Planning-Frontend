f5c306f55d16a9ee6bd47e354e723fea2db72f04
Your Full Name <harshal@mamotechnolabs.com>
2026-05-10 14:39:29 +0530

    added api

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://collaborative-trip-planning-api.onrender.com',
         changeOrigin: true,
       },
     },
